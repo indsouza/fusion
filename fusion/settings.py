538c2a87fc78b9c2deb902e24d54af419c2bd0d8
Igor Souza <igornevesdesouza@gmail.com>
2025-11-18 23:52:25 -0300

    Definindo os template com class based views, aprendendo herança com extends/include e definindo páginas de erro

--- fusion/settings.py
+++ fusion/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'fusion.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'fusion',
-        'USER': 'igor_souza',
-        'PASSWORD': 'catarina',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -121,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 MEDIA_URL = 'media/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 MEDIA_ROOT = os.path.join (BASE_DIR, 'media')
